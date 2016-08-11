574c1d3d8e20e377720352e146be52cc97e3b86b
SmilyArora <smily.arora96@gmail.com>
2016-08-11 14:03:12 +0530

    initial commit

---
+++ graphs/blog/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+from django.db import models
+import datetime
+
+TYPE_CHOICES = (
+        ('Bar Graph', 'Bar Graph'),
+        ('Line Graph', 'Line Graph'),
+        ('Pie Graph','Pie Graph'),
+    )
+
+class User(models.Model):
+    	name=models.CharField(max_length=30)
+    	type_Of_Graph=models.CharField(max_length=10,choices=TYPE_CHOICES,default=1)
+        x_Attribute=models.CharField(max_length=10)
+        y_Attribute=models.CharField(max_length=10)
+        file_csv=models.FileField(upload_to='files/',default='/no-file.csv')
+
+    	class Admin:
+    				pass
+
+    	def __str__(self):
+    		return '%s,%s,%s,%s,%s\n' % (self.x_Attribute,self.y_Attribute,self.file_csv,self.type_Of_Graph,self.name)
+
+        # def __str__(self):
+        #     return '%s' % (self.file_csv)
---
+++ graphs/views.py
@@ -0,0 +1,111 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+import datetime
+from django.utils.safestring import mark_safe
+from blog.forms import GraphForm
+from blog.models import User
+
+import csv
+import json
+
+import os
+
+json_data=[]
+
+def clear():
+	del json_data[:]
+
+def welcome(request):
+	return redirect('http://www.google.com')
+
+def adder(request):
+	if request.method=='POST':
+		form=GraphForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			return redirect('/convert/')
+	else:
+		form=GraphForm()
+		return render(request, 'ipform.html', {'form': form })
+
+def index(request):
+	return render(request, 'index.html')
+
+def loginportal(request):
+	return render(request, 'login.html')
+
+
+def converter(request):
+	a=User.objects.all() #Getting the data from the database.
+	row = a[len(a)-1] 	#Getting the latest value from the database.
+	row=str(row)
+	p='static/media/'
+	row=row.split(",") #This seperated the data.
+	global x
+	global y
+	global type_graph
+	global name_of_graph
+	x=row[0]
+	y=row[1]
+	p+=row[2]
+	type_graph=row[3]
+	name_of_graph=row[4]
+	p=p.replace('\n','')
+	jsonfilewr='static/media/files/'
+	print("yolo  debugging : " + x + y +" " + type_graph)
+	#Converting the data from CSV format to JSON format. JSON = Javascript Object Notation Formati
+	csvfile = open(p, 'r')
+	jsonfilewr+='djangojson.json'
+	jsonfile = open(jsonfilewr, 'w')
+	#json_data=[]
+	i=1
+	fieldnames = (x,y)
+	reader = csv.DictReader(csvfile,fieldnames)
+	for row in reader:
+		#json.dump(row, jsonfile)
+		#jsonfile.write(',\n')
+		#print ("" + str(row))
+		if i != 1:
+			json_data.append(row)
+		i=i+1
+
+
+	return redirect ('/success/')
+	#return render(request, 'graph.html', {"p":jsonfilewr})
+
+
+def hardcode(request):
+	domain_x_min=999999999999999999
+	domain_x_max=0
+	domain_y_min=999999999999999999
+	domain_y_max=0
+	x_axis=x
+	y_axis=y
+	name_x=x_axis
+	name_y=y_axis
+	name_graph=name_of_graph
+	#Code to find the minimum and maximum domain ranges.
+	for i  in range(0,len(json_data)):
+	    domain_x_max=max(domain_x_max,int(json_data[i][str(x_axis)]))
+
+	for i  in range(0,len(json_data)):
+	    domain_y_max=max(domain_y_max,int(json_data[i][str(y_axis)]))
+
+	for i in range(0,len(json_data)):
+		if int(json_data[i][str(x_axis)]) <= domain_x_min :
+			domain_x_min=int(json_data[i][str(x_axis)])
+
+	for i in range(0,len(json_data)):
+		if int(json_data[i][str(y_axis)]) <= domain_y_min :
+			domain_y_min=int(json_data[i][str(y_axis)])
+
+	print (str(type_graph))
+	p=json.dumps(json_data)
+
+	clear() #Clears the json_data else the data used to get appended.
+	if 'Bar Graph' in type_graph :
+		return render(request, "sampled3.html", locals()) #Locals = Sending the variables.
+	if 'Line Graph' in type_graph :
+		return render(request, "khatterd3.html", locals())
+	if 'Pie Graph' in type_graph :
+		return render(request, "bard3.html", locals())
